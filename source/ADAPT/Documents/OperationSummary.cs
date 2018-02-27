efb7403965ec217f90a05de95e61ea788f9c5079
Ashley Anderson <ashley.anderson@reedbusiness.com>
2018-02-27 11:47:19 +0000

    Compatibility updates

--- source/ADAPT/Documents/OperationSummary.cs
+++ source/ADAPT/Documents/OperationSummary.cs
@@ -4,7 +4,7 @@
   * All rights reserved. This program and the accompanying materials
   * are made available under the terms of the Eclipse Public License v1.0
   * which accompanies this distribution, and is available at
-  * http://www.eclipse.org/legal/epl-v10.html <http://www.eclipse.org/legal/epl-v10.html> 
+  * http://www.eclipse.org/legal/epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
   *
   * Contributors:
   *    Jospeh Ross - creating class
@@ -34,7 +34,7 @@ namespace AgGateway.ADAPT.ApplicationDataModel.Documents
         public int ProductId { get; set; }
 
         public int? WorkItemOperationId { get; set; }
-        
+
         public List<StampedMeteredValues> Data { get; set; }
 
         public List<int> EquipmentConfigurationIds { get; set; }
--- source/ADAPT/LoggedData/DataLogTrigger.cs
+++ source/ADAPT/LoggedData/DataLogTrigger.cs
@@ -4,12 +4,12 @@
   * All rights reserved. This program and the accompanying materials
   * are made available under the terms of the Eclipse Public License v1.0
   * which accompanies this distribution, and is available at
-  * http://www.eclipse.org/legal/epl-v10.html <http://www.eclipse.org/legal/epl-v10.html> 
+  * http://www.eclipse.org/legal/epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
   *
   * Contributors:
   *    Tarak Reddy, Tim Shearouse - initial API and implementation
   *    Stuart Rhea - #96 Remove SectionId from DataLogTrigger
-  *                  #97 Replace DataLogTrigger.MeterId with DataLogTrigger.DeviceElementId   
+  *                  #97 Replace DataLogTrigger.MeterId with DataLogTrigger.DeviceElementId
   *******************************************************************************/
 
 using System.Collections.Generic;
